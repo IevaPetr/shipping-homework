cd1e7d20ad310e708b952dafde338ec3e8d51fa8
IevaPetr <ievap@wix.com>
2021-07-23 16:33:29 +0300

    Add all files

--- src/main/java/com/wixpress/homework/PriceCalculator.java
+++ src/main/java/com/wixpress/homework/PriceCalculator.java
@@ -1,9 +1,63 @@
 package com.wixpress.homework;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
 public class PriceCalculator {
+    public static final String OM = "OM", DH = "DH", LP = "LP";
+
+    public static final String SMALL = "S", MEDIUM = "M", LARGE = "L";
+
+    public static final String OM_S = "1";
+    public static final String OM_L = "7.20";
+    public static final String DH_S = "0.90";
+    public static final String DH_M = "3.40";
+    public static final String LP_S = "1.23";
+    public static final String LP_M = "3";
+    public static final String LP_L = "7";
 
     public String getPrice(String transaction) {
+        String provider = transaction.substring(13, 15);
+        String size = transaction.substring(11, 12);
+        String price = "ERROR";
+
+        if (isValidDate(transaction)) {
+            price = getPrice(provider, size);
+        }
+        transaction += " " + price;
         return transaction;
     }
 
+    private boolean isValidDate(String order) {
+        String date = order.substring(0, 10);
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        format.setLenient(false);
+
+        try {
+            format.parse(date);
+        }
+        catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getPrice(String provider, String size) {
+        if (provider.equals(OM)) {
+            if (size.equals(SMALL)) return OM_S;
+            if (size.equals(LARGE)) return OM_L;
+        }
+        if (provider.equals(DH)) {
+            if (size.equals(SMALL)) return DH_S;
+            if (size.equals(MEDIUM)) return DH_M;
+        }
+        if (provider.equals(LP)) {
+            if (size.equals(SMALL)) return LP_S;
+            if (size.equals(MEDIUM)) return LP_M;
+            if (size.equals(LARGE)) return LP_L;
+        }
+        return "ERROR";
+    }
 }
--- src/test/java/com/wixpress/homework/PriceCalculatorTest.java
+++ src/test/java/com/wixpress/homework/PriceCalculatorTest.java
@@ -12,5 +12,11 @@ class PriceCalculatorTest {
         final PriceCalculator priceCalculator = new PriceCalculator();
 
         assertEquals("2021-07-15 S LP 1.23", priceCalculator.getPrice("2021-07-15 S LP"));
+        assertEquals("2021-02-30 M DH ERROR", priceCalculator.getPrice("2021-02-30 M DH"));   //feb 30th doesn't exist
+        assertEquals("2021-02-30 L DH ERROR", priceCalculator.getPrice("2021-02-30 L DH"));   //DH doesn't have L size
+        assertEquals("2018-04-31 M DH ERROR", priceCalculator.getPrice("2018-04-31 M DH"));   //april 31st doesn't exist
+        assertEquals("2021-06-30 L LP 7", priceCalculator.getPrice("2021-06-30 L LP"));
+        assertEquals("2020-01-01 M DH 3.40", priceCalculator.getPrice("2020-01-01 M DH"));
+        assertEquals("2020-01-01 M LD ERROR", priceCalculator.getPrice("2020-01-01 M LD"));   //wrong provider
     }
 }
